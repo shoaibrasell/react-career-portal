002514ac080e93ca687f26e2f27d609d3d25a764
rasel <shoaibullahrasel@gmail.com>
2024-03-05 20:51:30 +0600

    first time commit

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeaturedJobs = () => {
+  return (
+    <div className='text-center'>
+      <h2 className='text-5xl'>Featured Jobs</h2>
+      <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+    </div>
+  );
+};
+
+export default FeaturedJobs;
